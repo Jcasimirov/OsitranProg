7ac9d153fa7c3f28e28e10733d057723bdf5c14c
henry2411 <henry2411@gmail.com>
2015-03-26 22:07:10 +0000

    Modificacion de Metodos para Logueo

--- ViewController/src/com/ositran/util/ControlAcceso.java
+++ ViewController/src/com/ositran/util/ControlAcceso.java
@@ -16,71 +16,55 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 public class ControlAcceso {
-     
-    private  HttpServletRequest httpServletRequest=null;
-    private  FacesContext faceContext=null;
-    private int leerSesion;
-    private List<RolOpcionesVO> listaRolOpciones=new ArrayList<>();
-    private List<UsuarioVO> listaUsuarios=new ArrayList<>();
-    private int ingresarSesion;
-    private int eliminarSesion;
-    private int actualizarSesion;
-    private String parametroValidacion;
-    
-    public boolean validarSesion(int formulario){
-        try {
-           faceContext=FacesContext.getCurrentInstance();
-           httpServletRequest=(HttpServletRequest)faceContext.getExternalContext().getRequest();
-           HttpSession session = httpServletRequest.getSession();
-           listaUsuarios=(List<UsuarioVO>)session.getAttribute("listaUsuario");
-           listaRolOpciones=(List<RolOpcionesVO>)session.getAttribute("listaPermisos");
-        
-           for (RolOpcionesVO rolO:listaRolOpciones){
-               if (rolO.getMenId()==formulario){
-                   parametroValidacion="true";
-                   }
-               }
-           
-           if (!"true".equals(parametroValidacion)) {
-                   
-                   FacesContext context = FacesContext.getCurrentInstance();
-                   ExternalContext externalContext = context.getExternalContext();
-                   ServletContext servletContext = (ServletContext) context.getExternalContext().getContext();
-                   faceContext=FacesContext.getCurrentInstance();
-                   httpServletRequest=(HttpServletRequest)faceContext.getExternalContext().getRequest();
-                    String redirectPath = "/faces/ositran/logueo.xhtml";
-                    externalContext.redirect(servletContext.getContextPath() + redirectPath);
-               }
-           else {
-               
-               for (RolOpcionesVO rolOpcion:listaRolOpciones){
-                   if (rolOpcion.getMenId()==formulario){
-                       leerSesion=rolOpcion.getTroConsultar();
-                       ingresarSesion=rolOpcion.getTroAgregar();
-                       actualizarSesion=rolOpcion.getTroModificar();
-                       eliminarSesion=rolOpcion.getTroEliminar();
+    public static ControlAcceso newInstance = new ControlAcceso();   
+    private HttpServletRequest httpServletRequest = null;
+    private FacesContext faceContext = null;
+    private List<RolOpcionesVO> listaRolOpciones = new ArrayList<>();
+    private List<UsuarioVO> listaUsuarios = new ArrayList<>();
+    private boolean parametroValidacion = false;
 
-                       }
-                   }
-               } 
-            
+    public RolOpcionesVO validarSesion(int formulario) throws IOException {
+        RolOpcionesVO roles = new RolOpcionesVO();
+        try {
+            faceContext = FacesContext.getCurrentInstance();
+            httpServletRequest = (HttpServletRequest) faceContext.getExternalContext().getRequest();
+            HttpSession session = httpServletRequest.getSession();
+            listaUsuarios = (List<UsuarioVO>) session.getAttribute("listaUsuario");
+            listaRolOpciones = (List<RolOpcionesVO>) session.getAttribute("listaPermisos");
+
+            for (RolOpcionesVO rolO : listaRolOpciones) {
+                if (rolO.getMenId() == formulario) 
+                    parametroValidacion = true;                
             }
-        
-            catch(IOException io){
-            io.printStackTrace();
+            if (!parametroValidacion) {
+              logout();
+            } else {
+                for (RolOpcionesVO rolOpcion : listaRolOpciones) {
+                    if (rolOpcion.getMenId() == formulario) 
+                        return rolOpcion;                    
+                }
+
             }
-        catch (Exception ex){
-            ex.printStackTrace();
-            }
-        finally{
-            
-            
-            }
-        
-        
-        
-        
-        return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            logout();
         }
-    
+        return null;
+    }
+    public void logout()throws IOException{
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = context.getExternalContext();
+        ServletContext servletContext = (ServletContext) context.getExternalContext().getContext();
+        faceContext = FacesContext.getCurrentInstance();
+        httpServletRequest = (HttpServletRequest) faceContext.getExternalContext().getRequest();
+        String redirectPath = "/faces/ositran/logueo.xhtml";
+        externalContext.redirect(servletContext.getContextPath() + redirectPath);
+    }
+    public static void setNewInstance(ControlAcceso newInstance) {
+        ControlAcceso.newInstance = newInstance;
+    }
+
+    public static ControlAcceso getNewInstance() {
+        return newInstance;
+    }
 }
