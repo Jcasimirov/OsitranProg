b4f61be371da6351ec1cf13ac000b309933f9845
marvin.lorenzo <marvin.lorenzo@asistp.us>
2015-03-26 15:21:40 +0000

    MLORENZO: contratoCompromiso

---
+++ ViewController/src/com/ositran/vo/bean/ContratoCompromisoVO.java
@@ -0,0 +1,125 @@
+package com.ositran.vo.bean;
+public class ContratoCompromisoVO {
+    private String ccoAlcance;
+    private Long ccoNeto;
+    private Integer ccoId;
+    private Long ccoIgv;
+    private String ccoPeriodo;
+    private Integer ccoPlazo;
+    private Integer ccoTipoCambio;
+    private Long ccoTotal;
+    private Integer conId;
+    private Integer monId;
+    private Integer porIgv;
+    private Integer tccTipo;
+    private Integer tivId;
+
+    public ContratoCompromisoVO() {
+        super();
+    }
+
+    
+    public String getCcoAlcance() {
+        return ccoAlcance;
+    }
+
+    public void setCcoAlcance(String ccoAlcance) {
+        this.ccoAlcance = ccoAlcance;
+    }
+
+    public Long getCcoNeto() {
+        return ccoNeto;
+    }
+
+    public void setCcoNeto(Long ccoNeto) {
+        this.ccoNeto = ccoNeto;
+    }
+
+    public Integer getCcoId() {
+        return ccoId;
+    }
+
+    public void setCcoId(Integer ccoId) {
+        this.ccoId = ccoId;
+    }
+
+    public Long getCcoIgv() {
+        return ccoIgv;
+    }
+
+    public void setCcoIgv(Long ccoIgv) {
+        this.ccoIgv = ccoIgv;
+    }
+
+    public String getCcoPeriodo() {
+        return ccoPeriodo;
+    }
+
+    public void setCcoPeriodo(String ccoPeriodo) {
+        this.ccoPeriodo = ccoPeriodo;
+    }
+
+    public Integer getCcoPlazo() {
+        return ccoPlazo;
+    }
+
+    public void setCcoPlazo(Integer ccoPlazo) {
+        this.ccoPlazo = ccoPlazo;
+    }
+
+    public Integer getCcoTipoCambio() {
+        return ccoTipoCambio;
+    }
+
+    public void setCcoTipoCambio(Integer ccoTipoCambio) {
+        this.ccoTipoCambio = ccoTipoCambio;
+    }
+
+    public Long getCcoTotal() {
+        return ccoTotal;
+    }
+
+    public void setCcoTotal(Long ccoTotal) {
+        this.ccoTotal = ccoTotal;
+    }
+
+    public Integer getConId() {
+        return conId;
+    }
+
+    public void setConId(Integer conId) {
+        this.conId = conId;
+    }
+
+    public Integer getMonId() {
+        return monId;
+    }
+
+    public void setMonId(Integer monId) {
+        this.monId = monId;
+    }
+
+    public Integer getPorIgv() {
+        return porIgv;
+    }
+
+    public void setPorIgv(Integer porIgv) {
+        this.porIgv = porIgv;
+    }
+
+    public Integer getTccTipo() {
+        return tccTipo;
+    }
+
+    public void setTccTipo(Integer tccTipo) {
+        this.tccTipo = tccTipo;
+    }
+
+    public Integer getTivId() {
+        return tivId;
+    }
+
+    public void setTivId(Integer tivId) {
+        this.tivId = tivId;
+    }
+}
